6373c60267e973ca0a8ee3e03c3908400c466c56
Thiago Martins <thiago_valentim1@hotmail.com>
2022-12-04 13:10:03 -0300

    refactor: change test flags to boolean values

--- src/was_run.py
+++ src/was_run.py
@@ -1,12 +1,13 @@
 from test_case import TestCase
 class WasRun(TestCase):
   def __init__(self, name: str):
-    self.wasRun = None
-    self.wasSetup = None
+    self.wasRun: bool = None
+    self.wasSetup: bool = None
     super().__init__(name)
 
   def testMethod(self) -> None:
     self.wasRun = 1
 
   def setUp(self):
+    self.wasRun = False
     self.wasSetup = 1
