ba342c707da488caa87c6c564c23a8bccaf1b5af
Thiago Martins <thiago_valentim1@hotmail.com>
2022-12-04 10:35:23 -0300

    feat: ensure WasRun changes its flag to 1 when test method is executed

---
+++ src/test_was_run.py
@@ -0,0 +1,6 @@
+from was_run import WasRun
+
+test = WasRun("testMethod")
+print(test.wasRun)
+test.testMethod()
+print(test.wasRun)
---
+++ src/was_run.py
@@ -0,0 +1,6 @@
+class WasRun:
+  def __init__(self, name: str):
+    self.wasRun = None
+
+  def testMethod(self):
+    self.wasRun = 1
