d44ab9abf6f96ae2fe79b372c759daf128260d6e
Thiago Martins <thiago_valentim1@hotmail.com>
2022-12-04 13:12:42 -0300

    refactor: create TestCaseTest sut in setUp method

--- src/test_case_test.py
+++ src/test_case_test.py
@@ -2,15 +2,16 @@ from test_case import TestCase
 from was_run import WasRun
 
 class TestCaseTest(TestCase):
+  def setUp(self):
+    self.sut = WasRun("testMethod")
+
   def testRunning(self):
-    test = WasRun("testMethod")
-    test.run()
-    assert(test.wasRun)
+    self.sut.run()
+    assert(self.sut.wasRun)
 
   def testSetUp(self):
-    test = WasRun("testMethod")
-    test.run()
-    assert(test.wasSetup)
+    self.sut.run()
+    assert(self.sut.wasSetup)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
